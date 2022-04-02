0bac0b0563b40a51858d1f95123cb42339673197
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-04-02 17:32:14 +0530

    Update payment-success.php

--- payment-success.php
+++ payment-success.php
@@ -12,12 +12,9 @@ $msg = "";
     $cust_id=$_SESSION['user_id'];
     $ids = $_SESSION['last_updated_id'];
     $tables = $_SESSION['tables'];
-    // print_r($cust_id);die;
-    echo '<pre>';
     $sql = "SELECT * FROM $tables  WHERE `id` = $ids";
     $res = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($res);
-    print_r($row);
     $length = 18;
     $merchant_order_id=substr(str_shuffle(str_repeat($x='ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz', ceil($length/strlen($x)) )),1,$length);
     $merchant_trans_id= time();
@@ -45,28 +42,19 @@ $msg = "";
     $finaldata['amount'] = $amount;
     $finaldata['order_id'] = $order_id;
     $finaldata['razorpay_payment_id'] = $_POST['razorpay_payment_id']; 
-    // print_r( $finaldata);
       if(isset($_POST['razorpay_payment_id'])){
       $payment_details=json_encode($finaldata);
       $razorpay_payment_id = $_POST['razorpay_payment_id']; 
       $payment_status = 1;
-      
       $table = $_SESSION['tables'];
-      // $ids = $_SESSION['last_updated_id'];
       $sql="UPDATE $table SET payment_status = '$payment_status',payment_id = '$razorpay_payment_id', payment_details = '$payment_details' WHERE `id`=$ids";
       $rslt = $conn->query($sql);
       if(!empty($_SESSION['payment_2'])){
         unset($_SESSION['payment_2']);
-         // $use_id =  json_decode($_COOKIE['Cookie'],true); 
-         // $cust_id=$use_id['id'];
-         print_r($cust_id);
          $start_date = date('Y-m-d');
          $expire_date = date("Y-m-d",strtotime("+60 day"));
          $sql1 = "INSERT INTO `myc_subscription`(`cust_id`,`start_date`,`expire_date`) VALUES ('$cust_id','$start_date','$expire_date')";
-         print_r($sql1);
          $qrys=mysqli_query($conn,$sql1);
-         print_r($qrys);
-         die;
          if($qrys){
             $type = "EOT Crane";
             $added_on = date('Y-m-d');
@@ -78,18 +66,15 @@ $msg = "";
              setcookie("count",$runs,time() + (86400 * 30),"/");
             if($runs==0){
               $qrys2 = "INSERT INTO `myc_subscription_count`(`user_id`,`type`,`count`,`added_on`) VALUES ('$cust_id','$type','$count','$added_on')";
-              print_r($qrys2);
               $sqls=mysqli_query($conn,$qrys2);
-              print_r($sqls);die;
                 $qrys4 = "SELECT `id` FROM `myc_subscription_count` WHERE `user_id`='$cust_id'";
-                print_r($qrys4);
                $run4=mysqli_query($conn,$qrys4);
               $runss=mysqli_num_rows($run4);
                setcookie("count",$runss,time() + (86400 * 30),"/");
                   
             }
          }
-      }die;
+      }
     }
 ?>
 <!doctype html>
